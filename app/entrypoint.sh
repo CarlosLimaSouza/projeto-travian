dc6075f0f0a201fdaf0fdc17fc097fad9fbc45e4
Carlos <valhallalima@gmail.com>
2025-06-21 02:39:13 -0300

    correção entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,10 +1,10 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Inicia o cron em background
-# service cron start
+# Inicia o cron em background
+service cron start
 
-# # Roda seu app Python (ou qualquer outro comando)
-# python main.py
+# Roda seu app Python em background (se necessário)
+python main.py &
 
-# # Mantém o container vivo
-# tail -f /dev/null
+# Mantém o container vivo
+tail -f /dev/null
